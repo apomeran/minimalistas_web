086fa2fc381e5771dd2f37cd4a32d39e3935905e
lukkm <lmoscovicz@gmail.com>
2013-12-26 03:24:25 -0300

    Now we have bio

--- bio.php
+++ bio.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"> 
 <title></title>
 <link href="css/style.css" rel="stylesheet" type="text/css" />
 <link href="css/layout.css" rel="stylesheet" type="text/css" />
@@ -45,6 +45,89 @@
 		<a class="invisible" href="images/gallery/luky/04.jpg" rel="prettyPhoto[gallery_luky]"></a>
 		<a class="invisible" href="images/gallery/luky/05.jpg" rel="prettyPhoto[gallery_luky]"></a>
 		<a class="invisible" href="images/gallery/luky/06.jpg" rel="prettyPhoto[gallery_luky]"></a>
+		
+		<div>
+		<p><em>Banda de música Rock-Pop surgida en el año 2012.</em></p>
+
+<p>Minimalistas emerge como resultado de la coincidencia de cuatro amigos con similares curiosidades y diversos talentos.</p>
+
+<h4>Reunión:</h4>
+<br>
+<p>Jonathan Jusid y Lucas Moscovicz formaban parte de una banda llamada Touché, la cual estaba en búsqueda de un guitarrista. Dado el azar, se toparon con Alan Pomerantz, quien estaba tocando la guitarra en medio de un evento y quedaron impresionados por su estilo y habilidad, y le ofrecieron a que asista a un ensayo de Touché. Con el tiempo formaria parte de la formacion de la misma. 
+
+Uriel Kutner, por su parte, formaba parte de la banda Calibre 56 como cantante, tecladista y guitarrista. Jonathan y Lucas, ya estaban al tanto de las habilidades de Uriel como músico, ya que eran amigos de la secundaria, pero no fue hasta que en un ensayo de Touché pasaron una canción de Calibre 56 que Alan conoció artísticamente a Uriel. En ese momento tomaron la decisión de contactarse con Uriel y reunirse, cosa que sucede a finales del 2011, en Diciembre, naciendo así, el proyecto "Minimalistas". 
+</p>
+<h4>La Dama (2012):</h4>
+<br>
+<p>En los inicios del año 2012 se decide poner inicio oficialmente a la banda, empiezan a componer juntos decenas de canciones de las cuales luego seleccionarían las más sobresalientes para producirlas y grabarlas ellos mismos. El diseño del logo de la banda, formado por la conjunción de 2 claves de sol y dos claves de sol invertidas formando una M fue diseñado por la banda misma, en un ataque de creatividad e ideas.
+</p>
+<p>Se lanza el Facebook, el YouTube y el Twitter de la banda, los cuales terminan siendo sus herramientas más fundamentales para la difusión de sus canciones. 
+Siempre incoporan todas las redes sociales habidas y por haber.
+</p>
+<p>A partir de la formación de la banda, la estrategia de difusión siempre fue la misma; ir subiendo las canciones nuevas individualmente a medida que estas fueran terminadas de grabar, a razón de aproximadamente una canción por mes.
+</p>
+<p>De más está decir que todo el material editado y el proceso de grabacion de los temas, fue realizado por los integrantes de la banda, sin recurrir a terceros. Lo cual implica que Minimalistas participa tanto en la produccion artistica como técnica de sus grabaciones.
+</p>
+<p>El primer corte en ver la luz fue, el titulado "La Dama" en Junio, seguido por "Nueva Tinta" y Ausente". Un tiempo después ese mismo año serían subidos los temas "Una Flor", "Hoy" y "Saber Avanzar". Cada tema propone una nueva rítmica y estilo, tratando de demostrar que Minimalistas no puede ser encasillado en un único género, mas que el independiente, por su condición de auto gestionados.
+</p>
+<p>Al finalizar el 2012 deciden compilar estas canciones en un pseudo álbum titulado "La Dama".
+</p>
+
+<h4>Entre Canibales, Sueño Letal y el lanzamiento de la banda (2013): </h4>
+<br>
+<p>
+En Enero del 2013 Minimalistas graba un cover de la canción "Entre Canibales" de Soda Stereo y sube su primer video a Youtube. 
+Filmado, editado, producido y masterizado por ellos mismos.
+Más tarde ese año graba el sencillo "Sueño Letal" y es presentado en su facebook con gran aceptación del público.
+</p>
+<p>En una entrevista realizada en 2012 en FM Bahia Grande, ante la pregunta de que depararía el 2013 para Minimalistas, Lucas Moscovicz contestó: "Vamos a seguir con nuestra misma estrategia, hacer lo que más nos gusta. Y creemos venir bien, el tiempo nos dira como sigue esto!"
+</p>
+<p>A principios de 2013, la banda queda seleccionada entre mas de 300 bandas en el concurso "Seleccion de bandas 2013" auspiciado por Pepsi y consigue grabar su tema "La Dama" en el estudio "El Santito" producidos por Richard Coleman y gracias a su éxito en el estudio consiguen un lugar para tocar en vivo en el festival Pepsi Music 2013 el día Miercoles 3 de Abril en Costanera Sur, junto a grandes artistas como Pearl Jam.
+</p>
+<p>El 22 de Abril se presentó un nuevo tema, "Retrato al Despertar", con videoclip incluido, el cual fue realizado integramente y en su totalidad por la banda, nuevamente.
+</p>
+<p>Minimalistas resulta ganador del concurso de bandas de Canal Encuentro 'Mi Banda', entre mas de 200 bandas.
+</p>
+<p>Minimalistas graba un videoclip en vivo en los estudios de la facultad UADE en Julio del 2013 del tema Ausente, el mismo se puede encontrar en YouTube:
+<a href="http://www.youtube.com/watch?v=H5QqpObjAjo">http://www.youtube.com/watch?v=H5QqpObjAjo</a>
+</p>
+
+<p>
+Minimalistas a lo largo del 2013 realiza presentaciones de todo tipo y en todo lugar, entre las que se destacan: 
+<ul>
+<li>Casa Hillel 03.3.2013</li>
+<li>Fraga y Forest. 22.03.2013</li>
+<li>Teatro Acatraz 12.04.2013</li>
+<li>Álamo Bar 01.05.2013</li>
+<li>Caburé Bar 09.05.2013</li>
+<li>Garibaldi Bar 31.5.2013</li>
+<li>Especial VideoBar 21.06.2013</li>
+<li>Gallery Night Hillel 27.06.2013</li>
+</ul>
+<br>
+entre otras presentaciones, notas a radios, entrevistas, acústicos, etc.
+</p>
+<p>Minimalistas lanza su single "Mi Guia", una balada interesante que propone distintos matices y demuestra la versatilidad que tiene la banda a la hora de componer temas.
+Su última producción es la canción "Dejate Llevar" que sale a la luz a fines de Agosto del 2013.
+</p>
+<p>Minimalistas graba en Septiembre de 2013 para el canal EnsayoTV. Donde se realiza un videoclip y una nota a los musicos.
+</p>
+<p>Minimalistas participa de la inaguracion el 19.09.2013 de un Espacio Verde en Barracas junto al Gobierno de la Ciudad
+</p>
+<p>Minimalistas resulta finalista del concurso de Bandas Movistar Free Music 2013, presentandose en Samsung Studio el dia 8 de Noviembre, con una gran presentacion.
+</p>
+<p>Minimalistas realiza una nota para un programa conducido por Jimena Grandinetti (Conductora C5N 1a5), en donde realizan una entrevista y presentacion con temas acústicos.
+</p>
+<p>Minimalistas asiste a Radio Continental AM590 el 20.11 , para realizar una entrevista, contar un poco el balance del año 2013, y realizar una presentacion acustica. 
+</p>
+<p>Minimalistas participa como invitado para abrir el festival de bandas Tigre Rock 2013, en su día final numero 3.
+A los 4 dias, vuelve a abrir el festival ExpoTigre el 20 de Noviembre de 2013, en el estadio Pipa Tigre, en el Puerto de Frutos.
+</p>
+<p>Minimalistas resulta entre los 20 seleccionados para el concurso de bandas Speedy.
+</p>
+<p>Para cerrar el 2013 Minimalistas se presenta en el Marquee ante su público realizando una grabación de su primer DVD de un recital completo en vivo el cual se encontrará disponible para ver a principios de 2014.
+</p>
+		</div>
 	</div>
 	<div class="col-3">
 		<?php require('col3.php'); ?>
