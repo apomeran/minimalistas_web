1cc793ea42d237805a56b9bdd81921ce2c4c935f
Alan Pomerantz <alan_pomerantz@hotmail.com>
2013-12-28 16:10:46 -0300

    Added Like Box Widget on Col1

--- col1.php
+++ col1.php
@@ -1,3 +1,13 @@
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_LA/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<div class="fb-like-box" data-href="http://www.facebook.com/minimalistasmusica" data-width="80" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="false" data-show-border="false"></div>
+<br><br><br>
 <div class="p2">
 		<h2>La Dama</h2>
 		<img src="images/col-1-img1.png" class="p1" alt=""> <a href="#" class="more">Escuchar CD</a>
