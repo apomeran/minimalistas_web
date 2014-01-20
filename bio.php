822fa0635ed3ef4412559b23037876152098aeb2
Lucas Moscovicz <lmoscovicz@lmoscovicz-mbp.local>
2014-01-20 00:56:31 -0800

    Urka and Jony galleries working

--- bio.php
+++ bio.php
@@ -32,9 +32,9 @@
 	</div>
 	<div class="col-2">
 		<h2>Biografia</h2>
-		<div class="img-box fl thumbnail"> <a href="images/01.jpg" rel="prettyPhoto[gallery_urka]"><img src="images/gallery5.jpg" alt="Uriel Kutner"><br>
+		<div class="img-box fl thumbnail"> <a href="images/gallery/urka/01.jpg" rel="prettyPhoto[gallery_urka]"><img src="images/gallery/urka/thumb.png" alt="Uriel Kutner"><br>
 		Urka</a> </div>
-		<div class="img-box fl lst"> <a href="images/02.jpg" rel="prettyPhoto[gallery_jony]"><img src="images/gallery2.jpg" alt="Jonathan Jusid"><br>
+		<div class="img-box fl lst"> <a href="images/gallery/jony/01.jpg" rel="prettyPhoto[gallery_jony]"><img src="images/gallery/jony/thumb.png" alt="Jonathan Jusid"><br>
 		Jony</a> </div>
 		<div class="img-box fl"> <a href="images/gallery/luky/01.jpg" rel="prettyPhoto[gallery_luky]"><img src="images/gallery/luky/thumb.jpg" alt="Lucas Moscovicz"><br>
 		Luko</a> </div>
@@ -43,9 +43,20 @@
 		<a class="invisible" href="images/gallery/luky/02.jpg" rel="prettyPhoto[gallery_luky]"></a>
 		<a class="invisible" href="images/gallery/luky/03.jpg" rel="prettyPhoto[gallery_luky]"></a>
 		<a class="invisible" href="images/gallery/luky/04.jpg" rel="prettyPhoto[gallery_luky]"></a>
-		<a class="invisible" href="images/gallery/luky/05.jpg" rel="prettyPhoto[gallery_luky]"></a>
+		<a class="invisible" href="images/gallery/luky/05.png" rel="prettyPhoto[gallery_luky]"></a>
 		<a class="invisible" href="images/gallery/luky/06.jpg" rel="prettyPhoto[gallery_luky]"></a>
 		
+		<a class="invisible" href="images/gallery/jony/02.jpg" rel="prettyPhoto[gallery_jony]"></a>
+		<a class="invisible" href="images/gallery/jony/03.jpg" rel="prettyPhoto[gallery_jony]"></a>
+		<a class="invisible" href="images/gallery/jony/04.jpg" rel="prettyPhoto[gallery_jony]"></a>
+		<a class="invisible" href="images/gallery/jony/05.jpg" rel="prettyPhoto[gallery_jony]"></a>
+		<a class="invisible" href="images/gallery/jony/06.jpg" rel="prettyPhoto[gallery_jony]"></a>
+		
+		<a class="invisible" href="images/gallery/urka/02.jpg" rel="prettyPhoto[gallery_urka]"></a>
+		<a class="invisible" href="images/gallery/urka/03.jpg" rel="prettyPhoto[gallery_urka]"></a>
+		<a class="invisible" href="images/gallery/urka/04.jpg" rel="prettyPhoto[gallery_urka]"></a>
+		<a class="invisible" href="images/gallery/urka/05.jpg" rel="prettyPhoto[gallery_urka]"></a>
+		<a class="invisible" href="images/gallery/urka/06.jpg" rel="prettyPhoto[gallery_urka]"></a>
 		<div>
 		<p><em>Banda de música Rock-Pop surgida en el año 2012.</em></p>
 
